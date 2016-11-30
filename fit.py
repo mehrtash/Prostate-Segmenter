eae6e8547ceb30e928e01808d7ebf58c08210c49
Alireza Mehrtash <mehrtash@bwh.harvard.edu>
2016-11-30 11:04:08 -0500

    Updated fit to pass input and output files as arguments.

--- fit.py
+++ fit.py
@@ -1,5 +1,5 @@
 import os
-import sys
+import sys, getopt
 
 SEGMENTER_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'prostatesegmenter')
 print(SEGMENTER_PATH)
@@ -10,17 +10,39 @@ from prostatesegmenter.model import CNNModel
 from prostatesegmenter.nets import unet1
 from prostatesegmenter.segmenter import Segmenter
 
-if __name__ == '__main__':
-    ds = ProstateData()
-    model = unet1.model(weights=True, summary=True)
-    cnn = CNNModel(data_streamer=ds, model=model)
-    model_id = "unet1"
-    uid = '2016_07_13_17_56_21' + '_' + model_id
 
-    rows = 128
-    cols = 128
-    #
-    sg = Segmenter(cnn, uid)
-    image_file_name = '/home/mehrtash/github/DeepInfer-Client-build/input.nrrd'
-    label_file_name = '/home/mehrtash/github/DeepInfer-Client-build/output.nrrd'
-    sg.segment_prostate_volume(image_file_name, label_file_name, rows, cols)
+def main(argv):
+    inputfile = ''
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print 'usage: fit.py -i <inputfile> -o <outputfile>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'test.py -i <inputfile> -o <outputfile>'
+            sys.exit()
+        elif opt in ("-i", "--input"):
+            inputfile = arg
+        elif opt in ("-o", "--output"):
+            outputfile = arg
+    if inputfile == '' or outputfile == '':
+        print 'usage: fit.py -i <inputfile> -o <outputfile>'
+        sys.exit()
+    if os.path.isfile(inputfile) and os.path.isdir(os.path.dirname(outputfile)):
+        ds = ProstateData()
+        model = unet1.model(weights=True, summary=True)
+        cnn = CNNModel(data_streamer=ds, model=model)
+        rows = 128
+        cols = 128
+        #
+        sg = Segmenter(cnn)
+        sg.segment_prostate_volume(inputfile, outputfile, rows, cols)
+    else:
+        print "Make sure the input file exists and the output file directory is valid."
+        print "inputfile: ", inputfile
+        print "outputfile: ", outputfile
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
