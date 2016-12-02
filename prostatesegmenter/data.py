baed05b6a03191538737a1f6a4928e8d6c577e31
Alireza Mehrtash <mehrtash@bwh.harvard.edu>
2016-12-02 17:45:39 -0500

    Fixed data path.

--- prostatesegmenter/data.py
+++ prostatesegmenter/data.py
@@ -7,6 +7,6 @@ class ProstateData(object):
     def load_mean_train(self):
         dirname = os.path.dirname(__file__)
         fname = 'mean_train.npy'
-        datapath = os.path.join(dirname, '.', 'data', fname)
+        datapath = os.path.join(dirname, 'data', fname)
         data = np.load(datapath)
         return data
